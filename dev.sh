617b7c886b5007be3e92d0e57b84d590c23ca968
Andrew Jackson <andrew.jackson@dpconline.org>
2025-06-11 18:01:31 +0100

    Fix links to editor.

--- dev.sh
+++ dev.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-hugo --baseURL /bit-list/
+
+#export BASE_URL="/bit-list/"
+export BASE_URL="/"
+
+hugo --baseURL $BASE_URL
 
 npx pagefind --site public --output-path static/pagefind
 
 npx decap-server 2>/dev/null &
 
-hugo server -w --disableFastRender --baseURL //localhost:1313/bit-list/
+hugo server -w --disableFastRender --baseURL //localhost:1313$BASE_URL
