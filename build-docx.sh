0091b4ef39c008b290290917f33652bc90c16a13
Andrew Jackson <andrew.jackson@dpconline.org>
2025-07-30 14:37:34 +0100

    Use the source... assets.

--- build-docx.sh
+++ build-docx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 hugo
-pandoc public/entries/index.md --resource-path=./public --from=markdown+pipe_tables+link_attributes --dpi=300 --reference-doc=_pandoc/custom-reference.docx -o public/entries/index.docx
+pandoc public/entries/index.md --resource-path=./assets --from=markdown+pipe_tables+link_attributes --dpi=300 --reference-doc=_pandoc/custom-reference.docx -o public/entries/index.docx
 
